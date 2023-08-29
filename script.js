be91a5d0bde6cc7a577900cc5587e8bd1d8b7496
Jay <jaysun1967@gmail.com>
2023-08-29 21:13:45 +1000

    made button to change grid size. need to fix height of the container

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
 const gridContainer = document.querySelector('.grid-container')
 
-for (let i = 0; i < 16; i++) { 
-    const content = document.createElement('div');
-    content.classList.add('col');    
-    gridContainer.appendChild(content);
+function resetGrid() {
+    const cols = document.querySelectorAll('.col');
+    cols.forEach(element => {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+    })
 }
 
-const cols = document.querySelectorAll('.col');
-cols.forEach(element => {
-    for (let i = 0; i < 16; i++) { 
+function createGrid(input) {
+    const gridContainer = document.querySelector('.grid-container')
+
+    // Create columns
+    for (let i = 0; i < input - 1; i++) { 
         const content = document.createElement('div');
-        content.classList.add('cell');
-        content.textContent = i;
-        
-        element.appendChild(content);
+        content.classList.add('col');    
+        gridContainer.appendChild(content);
+    }
+
+    // Create cells in the grid
+    const cols = document.querySelectorAll('.col');
+    cols.forEach(element => {
+        for (let i = 0; i < input; i++) { 
+            const content = document.createElement('div');
+            content.classList.add('cell');
+
+            element.appendChild(content);
+        }
+    });
+
+    // Add event listens to each cell
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(element => {
+        // Add on mouseover event listers for each buton
+        element.addEventListener('mouseover', () => {
+            element.style.background =  '#ccc';
+        });
+    });
+}
+
+
+// Add event listeners to buttons
+const sizeBtn = document.querySelector('.size');
+sizeBtn.addEventListener('click', () => {
+    input = prompt("Please select the number of squares per side for the new grid.");
+
+    input = Number(input);
+    console.log(input)
+    if (isNaN(input)) {
+        alert("Please enter a number greater than 0 and less than 65")
+    }
+    else if (input > 64 || input < 1) {
+        alert("Please enter a number greater than 0 and less than 65")
+    }
+    else {
+        resetGrid(input);
+        createGrid(input);
     }
 });
+
+createGrid(16);
